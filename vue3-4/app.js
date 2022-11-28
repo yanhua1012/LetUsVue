2b5b3a906989d18f4e203b7687bb88041bb6c154
王彥驊 <terry.wang@iisigroup.com>
2022-11-28 14:31:29 +0800

    綁定鎮列資料:網站輪播圖

---
+++ vue3-4/app.js
@@ -0,0 +1,15 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+        slideList: [
+            { src: "./images/slide-01.jpg", desc: "1st"},
+            { src: "./images/slide-02.jpg", desc: "2st"},
+            { src: "./images/slide-03.jpg", desc: "3st"},
+            { src: "./images/slide-04.jpg", desc: "4st"},
+            { src: "./images/slide-05.jpg", desc: "5st"},
+        ]
+    },
+    methods: {
+       
+    }
+});
