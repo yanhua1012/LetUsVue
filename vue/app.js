d626dfbec9c96d5b585deb64dd448e91d5382e54
yanhua1012 <yanhua1012@gmail.com>
2022-11-28 03:05:12 +0000

    基本操作

--- vue/app.js
+++ vue/app.js
@@ -1,6 +1,13 @@
 var vm = new Vue({
     el: "#app",
     data: {
-        message: "Hello World!"
+        message: "Hello World!",
+        list: ["王大明", "劉小花", "歐陽大大"],
+        isShow: false
+    },
+    methods: {
+        test() {
+            alert("Hi");
+        }
     }
 });
